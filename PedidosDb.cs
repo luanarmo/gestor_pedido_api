cc3f77b1f65ebdcf49a635ef14d47ba014c4dfe1
luanarmo <luisangel_arroyo@outlook.com>
2024-10-13 22:55:44 -0600

    Creacion y definicion del contexto de base de datos

---
+++ PedidosDb.cs
@@ -0,0 +1,17 @@
+﻿
+namespace gestorPedidoAPI
+{
+    using Microsoft.EntityFrameworkCore;
+    public class PedidosDb(DbContextOptions<PedidosDb> options) : DbContext(options)
+    {
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<PedidoProducto> PedidoProductos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PedidoProducto>()
+                .HasKey(pp => new { pp.PedidoId, pp.ProductoId });
+        }
+    }
+}
