ab6dea85152cfd0048c5f60bb4a1bcda2812e15a
luanarmo <luisangel_arroyo@outlook.com>
2024-10-13 22:54:47 -0600

    Creacion y definicion de PedidoProductoDTO

---
+++ PedidoProductoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gestorPedidoAPI
+{
+    public class PedidoProductoDTO
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "El ID del producto debe ser un número positivo.")]
+        public int Producto { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser un número positivo.")]
+        public int Cantidad { get; set; }
+
+        public PedidoProductoDTO() { }
+        public PedidoProductoDTO(PedidoProducto pedidoProducto) => (Producto, Cantidad) = (pedidoProducto.ProductoId, pedidoProducto.Cantidad);
+    }
+}
