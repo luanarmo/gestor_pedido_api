97dab4c4fc225c3159a74f68687b979b04e819de
luanarmo <luisangel_arroyo@outlook.com>
2024-10-13 22:51:56 -0600

    Creacion y definicion del modelo Pedido

---
+++ Pedido.cs
@@ -0,0 +1,10 @@
+﻿namespace gestorPedidoAPI
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public int ClienteId { get; set; }
+        public List<PedidoProducto>? PedidoProductos { get; set; }
+    }
+}
