4c6db03fafe792737318631dd67b7cf96c2466bd
luanarmo <luisangel_arroyo@outlook.com>
2024-10-13 22:53:47 -0600

    Creacion y definicion del modelo PedidoProducto

---
+++ PedidoProducto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace gestorPedidoAPI
+{
+    public class PedidoProducto
+    {
+        public int Id { get; set; }
+        public int PedidoId { get; set; }
+        public Pedido? Pedido { get; set; }
+        public int ProductoId { get; set; }
+        public Producto? Producto { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
